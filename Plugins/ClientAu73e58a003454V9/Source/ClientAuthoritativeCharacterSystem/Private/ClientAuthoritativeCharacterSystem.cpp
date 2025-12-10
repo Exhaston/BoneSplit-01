ebdca2bc58a9a24c6aeee05dc4896cb8908cf6e0
Einar Birkeland <einarjohanneslb97@gmail.com>
2025-12-10 04:11:10 +0100

    Temp remove plugin before gitignore update

--- Plugins/ClientAu73e58a003454V9/Source/ClientAuthoritativeCharacterSystem/Private/ClientAuthoritativeCharacterSystem.cpp
+++
@@ -1,6 +0,0 @@
-﻿// Copyright 2023 Anton Hesselbom. All Rights Reserved.
-
-#include "ClientAuthoritativeCharacterSystem.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_MODULE(FDefaultModuleImpl, ClientAuthoritativeCharacterSystem);
--- Plugins/ClientAu73e58a003454V9/Source/ClientAuthoritativeCharacterSystem/Public/ClientAuthoritativeCharacterSystem.h
+++
@@ -1,5 +0,0 @@
-﻿// Copyright 2023 Anton Hesselbom. All Rights Reserved.
-
-#pragma once
-
-#include "CoreMinimal.h"
