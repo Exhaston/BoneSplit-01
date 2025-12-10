af85849e20b56aa10f4caf2a3f592fcbb2b16db8
Einar Birkeland <einarjohanneslb97@gmail.com>
2025-12-10 04:13:35 +0100

    Re-Adding plugins to project

---
+++ Plugins/ClientAu73e58a003454V9/Source/ClientAuthoritativeCharacterSystem/Public/ClientAuthoritativeCharacterSystem.h
@@ -0,0 +1,5 @@
+﻿// Copyright 2023 Anton Hesselbom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
---
+++ Plugins/ClientAu73e58a003454V9/Source/ClientAuthoritativeCharacterSystem/Public/ClientAuthoritativePlayerController.h
@@ -0,0 +1,17 @@
+﻿// Copyright 2023 Anton Hesselbom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ClientAuthoritativePlayerController.generated.h"
+
+// Intended to be used together with AClientAuthoritativeCharacter & UClientAuthoritativeCharacterMovementComponent
+UCLASS()
+class CLIENTAUTHORITATIVECHARACTERSYSTEM_API AClientAuthoritativePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	virtual void TickActor(float DeltaTime, ELevelTick TickType, FActorTickFunction& ThisTickFunction) override;
+};
