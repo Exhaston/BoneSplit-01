00856106682e8bc3b3608d11dfe7a268f22995e3
Einar Birkeland <einarjohanneslb97@gmail.com>
2026-01-26 15:40:21 +0100

    Refactoring Interfaces

--- Source/BoneSplit/Private/Components/AbilitySystem/BSKillableInterface.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) 2025 Electric Jolt. All Rights Reserved.
-
-
-#include "Components/AbilitySystem/BSKillableInterface.h"
-
-
-// Add default functionality here for any IBSKillable functions that are not pure virtual.
